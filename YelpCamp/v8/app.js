f5ffd2c801c9ec76870ef8cb5c6a53743524447c
dylan <d.hedges1@hotmail.co.uk>
2017-10-27 17:05:27 +0000

    YelpCamp - Associating users and comments

---
+++ YelpCamp/v8/app.js
@@ -0,0 +1,97 @@
+//--------------------SET UP--------------------
+//Instead of writing "var" over and over we can just use a "," and list all required
+var express         = require("express"),
+    app             = express(),
+    bodyParser      = require("body-parser"),
+    mongoose        = require("mongoose"),
+    passport        = require("passport"),
+    LocalStrategy   = require("passport-local"),
+    Campground      = require("./models/campground"),
+    Comment         = require("./models/comment"),
+    User            = require("./models/user"),
+    seedDB          = require("./seeds");
+
+//Requires our routes
+var commentRoutes  = require("./routes/comments"),
+    campgroundRoutes = require("./routes/campgrounds"),
+    indexRoutes      = require("./routes/index")
+
+
+//Mongodb connection - connect to and create the db (if not already created, ", {useMongoClient: true}" is optional and removes a warning )
+mongoose.connect("mongodb://localhost/yelp_camp_v3", {useMongoClient: true});
+//S27.5 in course, replaces Mongoose's default promise library with JS's native promise library
+mongoose.Promise = global.Promise;
+
+//Convert body (string) to a JS object
+app.use(bodyParser.urlencoded({extended: true}));
+//Set .ejs as the default extention for our files
+app.set("view engine", "ejs");
+//Lets the app access files in the public folder (css, JS), "__dirname" automatically refers to the directory path app.js was run in
+app.use(express.static(__dirname + "/public"));
+
+//Executes the above seedDB which has a function passed from seeds.js that deletes all campgrounds
+//seedDB();
+
+
+//-----PASSPORT CONFIG-----
+app.use(require("express-session")({
+    secret: "THIS IS USED TO ENCODE",
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+//"".authenticate" method comes with passport local mongoose, used as middleware for login/sign up
+passport.use(new LocalStrategy(User.authenticate()));
+//Takes decoded data, encodes it and puts it back into the session (method comes with passport local mongoose)
+passport.serializeUser(User.serializeUser());
+//Takes data from the session and decodes it (method comes with passport local mongoose)
+passport.deserializeUser(User.deserializeUser());
+
+
+//"currentUser: req.user" - take req.user and store in variable currentUser, when logging in passport will create req.user which contains the user ID and username (used to determine what to show in the nav bar e.g "Login" and "Sign up" or "Logout")
+//Using this means we dont have to specify "currentUser: req.user" in every route, it will be passed on every route
+app.use(function(req, res, next){
+    res.locals.currentUser = req.user;
+    //Proceed to the callback, nothing we happen next if we dont state this
+    next();
+});
+
+
+
+//Uses our route pages (required above)
+app.use(indexRoutes);
+app.use(campgroundRoutes);
+app.use(commentRoutes);
+
+
+//--------------------LISTENER--------------------
+app.listen(process.env.PORT, process.env.IP, function(){
+   console.log("YelpCamp Server has startd") 
+});
+
+
+
+
+
+
+
+
+//-------------IRRELEVANT CODE (FYI)-------------------
+
+//For the model "Campground" call the ".create" function and create a new camp
+// Campground.create({
+//     name: "Granite Hill", 
+//     image: "http://www.photosforclass.com/download/7878880968",
+//     description: "This is a huge granite hill, no bathrooms, no water, beautiful granite"
+// }, function(err, campground){
+//     //If there is an error print the error
+//     if(err){
+//         console.log(err)
+//     //else display text and campground info (function is always executed)    
+//     } else {
+//         console.log("NEWLY CREATED CAMPGROUND");
+//         console.log(campground);
+//         }
+//     }
+// );
---
+++ YelpCamp/v8/routes/comments.js
@@ -0,0 +1,67 @@
+//Instead of creating a new "var app" we are going to reference the exisiting one in the app.js file using express router
+var express = require("express");
+//Create a router variable with a new instance of express
+var router = express.Router();
+//Imports our campground and comment models which are referenced below
+var Campground = require("../models/campground");
+var Comment = require("../models/comment");
+
+
+
+
+//NEW COMMENT FORM ROUTE
+//Nested Routes - both routes require us to get the campground id first
+//"isLoggedIn" - a function (middleware) we created that checks if the user is logged in and only executes the next/callback function if they are
+router.get("/campgrounds/:id/comments/new", isLoggedIn, function(req, res){
+   Campground.findById(req.params.id, function(err, campground){
+       if(err){
+           console.log(err);
+       }else{
+           //{campground: campground} pass in campground from the DB as an object into the comments/new page
+           res.render("comments/new", {campground: campground});
+       }
+   })
+});
+
+//CREATE COMMENT ROUTE 
+//"isLoggedIn" - we also use it here to prevent people creating comments as a post request (e.g with postman)
+router.post("/campgrounds/:id/comments", isLoggedIn, function(req, res){
+   Campground.findById(req.params.id, function(err, campground){
+       if(err){
+           console.log(err);
+           res.redirect("/campgrounds");
+       }else{
+           //Create the comment (take directly from body when form is submitted)
+            Comment.create(req.body.comment, function(err,comment){
+                if (err){
+                    console.log(err);
+                }else{
+                    //Associates user id to comment - when a comment is created we take "req.user._id" and put it into "comment.author.id" (schema defined in "comment.js") 
+                    comment.author.id = req.user._id;
+                    //Assocaites username to comment
+                    comment.author.username = req.user.username;
+                    //Saves the comment (including username)
+                    comment.save();
+                    //Push comment into campground
+                    campground.comments.push(comment);
+                    //Save to DB
+                    campground.save();
+                    res.redirect('/campgrounds/' + campground._id);
+                }
+            });
+       }
+   })    
+});
+
+//Middleware
+function isLoggedIn(req, res, next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.redirect("/login");
+}
+
+
+
+//Exports the code to app.js
+module.exports = router;
